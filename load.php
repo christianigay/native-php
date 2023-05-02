e014bd7f10ce401d2504d4f25d1762fe434e3315
christian igay <christian.igay@gmail.com>
2023-05-03 07:59:02 +0800

    add load file

---
+++ load.php
@@ -0,0 +1,5 @@
+<?php
+require_once(__DIR__ . '/config/constant.php');
+require_once(__DIR__ . '/config/general_helper.php');
+ini_set('display_errors', ENVIRONMENT == 'development');
+require_once(__DIR__ . '/vendor/autoload.php');
