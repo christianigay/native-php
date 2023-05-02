d0cd5ee611a7eb50e5aba7845047b4fc9a5d8876
christian igay <christian.igay@gmail.com>
2023-05-03 07:58:36 +0800

    add constant

---
+++ config/constant.php
@@ -0,0 +1,8 @@
+<?php
+defined('ENVIRONMENT') || define('ENVIRONMENT', 'development');
+defined('BASE_URL') || define('BASE_URL', 'http://localhost:8189');
+defined('ROOT_DIRECTORY') || define('ROOT_DIRECTORY', $_SERVER['DOCUMENT_ROOT']);
+defined('DB_HOST') || define('DB_HOST', 'mysql');
+defined('DB_USERNAME') || define('DB_USERNAME', 'root');
+defined('DB_PASSWORD') || define('DB_PASSWORD', 'password');
+defined('DB_NAME') || define('DB_NAME', 'native_php');
